3b0f6003d40e22ed1293b553d9f3adc016abe1a3
vhnguy1142 <scientificsalmagundi@gmail.com>
2022-12-30 00:20:09 -0600

    Initial commit.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Dot_Net_To_Do_List.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Models/Task.cs
@@ -0,0 +1,12 @@
+﻿namespace Dot_Net_To_Do_List.Models
+{
+    public class Task
+    {
+        public int ID { get; set; }
+        
+        public string Name { get; set; }
+        public Boolean IsCompleted { get; set; }
+        public DateTime CreationDate { get; set; }
+    }
+
+}
